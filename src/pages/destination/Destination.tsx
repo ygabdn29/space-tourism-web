2eedd330bba6bffb94a521c866e206bea07cd8e5
Yoga <yogabudiman07@gmail.com>
2024-04-20 21:56:46 +0700

    style: responsiveness to tablet

--- src/pages/destination/Destination.tsx
+++ src/pages/destination/Destination.tsx
@@ -21,25 +21,28 @@ function Destination() {
   const { getDestination } = useData();
   const destination = getDestination();
 
-  useEffect(function () {
-    setIsActive(destination[0]);
-  }, []);
+  useEffect(
+    function () {
+      setIsActive(destination[0]);
+    },
+    [destination]
+  );
 
   return (
-    <div className="lg:max-w-[90rem] lg:mt-20 lg:ml-[10.5rem] lg:mr-[10.25rem]">
-      <h1 className="uppercase font-barlow-condensed text-white lg:mb-24 lg:text-[1.75rem] lg:tracking-[0.17em]">
+    <div className="lg:max-w-[90rem] md:max-w-[48rem] lg:mt-20 md:mt-10 lg:ml-[10.5rem] lg:mr-[10.25rem]">
+      <h1 className="uppercase font-barlow-condensed text-white lg:mb-24 md:mb-14 md:ml-10 lg:text-[1.75rem] md:text-[1.25rem] lg:tracking-[0.17em] md:tracking-[0.17em]">
         <span className="font-bold opacity-25 lg:mr-7  ">01</span>
         Pick Your Destination
       </h1>
 
-      <div className="flex lg:gap-40">
+      <div className="flex lg:flex-row md:flex-col lg:gap-40 md:gap-[3.375rem] lg:px-0 md:px-[6.125rem]">
         <img
           src={`${destination[currentDestination]["images"]["webp"]}`}
           alt={`Image of ${destination[currentDestination]["name"]}`}
-          className="lg:w-[27.8125rem]  lg:h-[27.8125rem]"
+          className="lg:w-[27.8125rem] md:w-[18.75rem] lg:h-[27.8125rem] md:h-[18.75rem] md:self-center"
         />
-        <div className="flex lg:flex-col">
-          <ul className="flex text-white lg:gap-9 lg:mb-9">
+        <div className="flex flex-col">
+          <ul className="flex text-white lg:gap-9 md:gap-9 lg:mb-9 lg:justify-start md:justify-center">
             {destination.map((dest: Destinations, i) => (
               <li
                 key={dest.name}
@@ -47,10 +50,10 @@ function Destination() {
                   setCurrentDestination(i);
                   setIsActive(dest);
                 }}
-                className={`transition-colors duration-300 lg:pb-3 ${
+                className={`transition-colors duration-300 lg:pb-3 md:pb-4 ${
                   isActive === dest
-                    ? "lg:border-b-4 lg:-mb-1 lg:border-white"
-                    : "lg:hover:border-b-4 lg:hover:border-white-05 lg:hover:-mb-1"
+                    ? "lg:border-b-4 md:border-b-4 lg:-mb-1 md:-mb-1 lg:border-white md:border-white"
+                    : "lg:hover:border-b-4 md:hover:border-b-4 lg:hover:border-white-05 md:hover:border-white-05 lg:hover:-mb-1 md:hover:-mb-1"
                 }`}
               >
                 {dest.name.toUpperCase()}
@@ -58,32 +61,32 @@ function Destination() {
             ))}
           </ul>
 
-          <h2 className="font-bellefair lg:mb-3 text-white uppercase lg:text-[6.25rem]">
+          <h2 className="font-bellefair lg:mb-3 text-white uppercase lg:text-[6.25rem] md:text-[5rem] lg:text-left md:text-center">
             {destination[currentDestination]["name"]}
           </h2>
 
-          <p className="font-barlow text-light-blue lg:text-lg lg:mb-[3.375rem]">
+          <p className="font-barlow text-light-blue lg:text-lg md:text-base lg:mb-[3.375rem] md:mb-12 lg:text-left md:text-center">
             {destination[currentDestination]["description"]}
           </p>
-          <hr className="lg:mb-7" />
+          <hr className="lg:mb-7 md:mb-7" />
 
-          <div className="flex lg:gap-20">
+          <div className="flex lg:gap-20 md:gap-3 md:justify-around">
             <div>
-              <p className="font-barlow text-sm text-light-blue tracking-[0.17em]">
+              <p className="font-barlow text-sm text-light-blue tracking-[0.17em] lg:text-left md:text-center">
                 Avg. Distance
               </p>
 
-              <p className="font-bellefair uppercase text-white text-[1.75rem]">
+              <p className="font-bellefair uppercase text-white text-[1.75rem] lg:text-left md:text-center">
                 {destination[currentDestination]["distance"]}
               </p>
             </div>
 
             <div>
-              <p className="font-barlow text-sm text-light-blue tracking-[0.17em]">
+              <p className="font-barlow text-sm text-light-blue tracking-[0.17em] lg:text-left md:text-center">
                 Est. Travel Time
               </p>
 
-              <p className="font-bellefair uppercase text-white text-[1.75rem]">
+              <p className="font-bellefair uppercase text-white text-[1.75rem] lg:text-left md:text-center">
                 {destination[currentDestination]["travel"]}
               </p>
             </div>
